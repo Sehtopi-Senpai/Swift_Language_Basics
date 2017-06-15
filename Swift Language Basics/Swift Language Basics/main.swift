9e5e1167fc6f470116270966615e7673aa01f5b7
Josh Green <jg229@mail.missouri.edu>
2017-06-15 15:49:39 -0500

    Added the Swift project files to the repository.

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Lappy on 6/15/17.
+//  Copyright © 2017 xanda code. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
